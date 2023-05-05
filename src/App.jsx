883b1bd0b720251bd3983fdc5bd033ac5f41bd78
nicodenelliGH <ndm9412@gmail.com>
2023-05-05 15:33:20 -0400

    adding login and signup functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
 
-import LoginPage from "./pages/LoginPage/LoginPage";
+import { useState } from "react";
 
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<h1>Home Pageeeeeeeeeee</h1>} />
-      <Route path="/login" element={<LoginPage />} />
-    </Routes>
-  );
+
+import LoginPage from "./pages/LoginPage/LoginPage";
+import SignupPage from './pages/SignupPage/SignupPage';
+
+import userService from './utils/userService';
+
+
+
+export default function App() {
+
+  const [user, setUser] = useState(userService.getUser())
+
+  function handleSignUpOrLogin(){
+    setUser(userService.getUser())
+  }
+
+    // are we logged in?
+    return (
+      <Routes>
+        <Route
+          path="/login"
+          element={<LoginPage handleSignUpOrLogin={handleSignUpOrLogin} />}
+        />
+        <Route
+          path="/signup"
+          element={<SignupPage handleSignUpOrLogin={handleSignUpOrLogin} />}
+        />
+      </Routes>
+    );
 }
 
-export default App;
